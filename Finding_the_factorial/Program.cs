432fcae3c314bcaa34aee46c6b47c80a922ec320
Anton <tepa-86@mail.ru>
2022-10-04 08:47:45 +0300

    add Finding_the_factorial

---
+++ Finding_the_factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿//считет максимальное 20
+
+string s = String.Empty;
+Console.WriteLine(s);
+Console.Write("Введите значение факториала: ");
+
+long factorial = Convert.ToInt64(Console.ReadLine());
+long current = 1;
+int miltiplier = 1;
+
+if (factorial == 0)
+{
+    Console.WriteLine($"Факториал равен: {current}");
+    Console.WriteLine(s);
+}
+
+for (long m = miltiplier; m <= factorial; m++)
+
+    {
+        current = current * m;
+    }
+    
+Console.WriteLine($"Факториал равен: {current}");
+Console.WriteLine(s);
+
+
